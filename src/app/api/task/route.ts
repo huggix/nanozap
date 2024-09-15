53d13b98addab8e7d3302b4282e36ca9838fce9c
Nicolai Lissau <nicolai@lissau.cloud>
2024-09-15 21:32:04 +0200

    Updating example with relevant data

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -15,7 +15,10 @@ export async function GET(request: Request) {
     
     const data = await checkAuth(task_id, password)
 
-    return NextResponse.json([data], { status: 200 });
+    return NextResponse.json([{
+      monitored_nano_address: data.monitored_nano_address || "nano_1w9mxfsd8e7mrksw3pa148n9kpzuafyz4j1ebx848mfxh3wirbsqa7rqf4ci",
+      known_nano_addresses: data.known_nano_addresses || ["nano_jdks9r8fncga7s63hdksid8n9kpzuafyz4j1ebx848mfxh3irbsqa7r0920a", "nano_8dks93jhd8e7m2ksw7pa148n9pzuafyz4j1ebx848mfxh3wirbsldox0laka"] ,
+    }], { status: 200 });
 
   } catch (error) {
     const e = error as Error || { message: 'Unknown error' }
