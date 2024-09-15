0ada3ce513809aac60a3ba4001719bcdaea06617
Nicolai Lissau <nicolai@lissau.cloud>
2024-09-15 21:45:29 +0200

    Remove loading data in example. Just return example

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,23 +1,12 @@
 import { NextResponse } from "next/server";
-import { checkAuth } from "@/lib/checkAuth"
 
-export async function GET(request: Request) {
-    
-  const {searchParams} = new URL(request.url);
-  const task_id = searchParams.get("task_id");
-  const password = searchParams.get("password");
+export async function GET() {
 
   try {
 
-    if (!task_id) {
-      throw new Error('Task id is required')
-    }
-    
-    const data = await checkAuth(task_id, password)
-
     return NextResponse.json([{
-      monitored_nano_address: data.monitored_nano_address || "nano_1w9mxfsd8e7mrksw3pa148n9kpzuafyz4j1ebx848mfxh3wirbsqa7rqf4ci",
-      known_nano_addresses: data.known_nano_addresses || ["nano_jdks9r8fncga7s63hdksid8n9kpzuafyz4j1ebx848mfxh3irbsqa7r0920a", "nano_8dks93jhd8e7m2ksw7pa148n9pzuafyz4j1ebx848mfxh3wirbsldox0laka"] ,
+      monitored_nano_address: "nano_1w9mxfsd8e7mrksw3pa148n9kpzuafyz4j1ebx848mfxh3wirbsqa7rqf4ci",
+      known_nano_addresses: ["nano_jdks9r8fncga7s63hdksid8n9kpzuafyz4j1ebx848mfxh3irbsqa7r0920a", "nano_8dks93jhd8e7m2ksw7pa148n9pzuafyz4j1ebx848mfxh3wirbsldox0laka"] ,
     }], { status: 200 });
 
   } catch (error) {
